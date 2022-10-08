1377bdecf157b481db04c97a40ba551952323e4b
Mjr4 <estmvega4@gmail.com>
2022-10-07 22:46:59 -0500

    starting proyect

---
+++ index.js
@@ -0,0 +1,10 @@
+const verify = document.querySelector(".btm")
+const text = document.querySelector("expr")
+
+verify.addEventListener("click",()=>{
+    console.log(text)
+})
+
+let userExpr = "if"
+
+
