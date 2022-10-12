81b0289bfee96109ca3884263d46a3a303c6cdea
Mjr4 <estmvega4@gmail.com>
2022-10-11 22:18:28 -0500

    upgrade regex for if statement

--- checkExp.js
+++ checkExp.js
@@ -1,16 +1,17 @@
 //const expIf = new RegExp("ab");
-const expVarName = /\D(\D|\d|-|_)*/; // equivalente a ^[a-zA-Z]([a-zA-Z]|[0-9]|-)*
-const expGetIfPar = /(?<vn>\w+)\s*(?<op>(>|<|==|<=|>=))\s*(?<con>\w+)/; //expresion obtener parametros if
-const expIfDecl = /if\s*\(\s*\w+\s*(>|<|==|<=|>=)\s*\w+\s*\)\:/; //exp declaracion if
-
+const expVarName = /^\D(\D|\d|-|_)*/; // equivalente a ^[a-zA-Z]([a-zA-Z]|[0-9]|-)*
+const expGetIfPar = /(?<vn>(\w+|\"(\w*\s*)*\"))\s*(?<op>(>|<|==|<=|>=))\s*(?<con>(\w+|\"(\w*\s*)*\"))/; //expresion obtener parametros if
+const expIfDecl = /if\s*\(\s*(\w+|\"(\w*\s*)*\")\s*(>|<|==|<=|>=)\s*(\w+|\"(\w*\s*)*\")\s*\)\:/; //exp declaracion if
+const expStrg = /^\"\w*|\s*\"/
 //const input = 'if(abc==8):'
 
 function chcVarName(varName) {
   return expVarName.test(varName);
 }
 
-function alex(inp = "") {
-  let match = expGetIfPar.exec(inp);
+
+function getIfparam(syntax){
+  let match = expGetIfPar.exec(syntax);
   let comp1 = match.groups.vn;
   let opert = match.groups.op;
   let comp2 = match.groups.con;
@@ -18,15 +19,32 @@ function alex(inp = "") {
   return [comp1, opert, comp2];
 }
 
-function checkImp(input) {
-  if (!expIfDecl.test(input)) {
-    console.log("invalid declaration of syntax if");
-    return 0;
-  }
-  console.log("valid declaration of syntax if");
-  const [a, b, c] = alex(input);
-  console.log("comparador 1: " + a, "operador: " + b, " comparador 2: " + c);
-  return 1;
+
+
+function validStrgs(inp){
+  return expStrg.test(inp)
 }
 
-checkImp("if(v>3):");
+export function identifSyntx(input) {
+  if (expIfDecl.test(input)) {
+    const [comp1, opert, conp2] = getIfparam(input)
+    
+
+    if(chcVarName(comp1) && chcVarName(conp2)){
+      return 'correct syntax for IF statemen'
+    }else if (validStrgs(comp1) && validStrgs(conp2) && opert=='==') {
+      return 'correct'
+    }else if (chcVarName(comp1) && validStrgs(conp2) && opert=='=='){
+      return 'correct'
+    }else if (validStrgs(comp1) && chcVarName(conp2) && opert=='=='){
+      return 'correct'
+    }
+
+    return 'incorrect syntax for IF statemen'
+  }
+  return 'not if statemen'
+}
+
+
+//console.log(identifSyntx('if (b == "a"):'))
+//console.log(chcVarName('1a'))
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import {variable} from './checkExp.js';
+import {identifSyntx} from './checkExp.js';
 
 const verify = document.querySelector("#btm");
 const text = document.querySelector("#expr");
 const form = document.querySelector("#form");
-
+const resp = document.querySelector("#respons");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(variable(text.value));
+  console.log(text.value);
+  resp.innerHTML = identifSyntx(text.value);
+  
 });
 
