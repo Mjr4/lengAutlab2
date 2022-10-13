604c15a2502d5eebd9473a567b7f1a3e20a5d740
Mjr4 <estmvega4@gmail.com>
2022-10-13 12:00:05 -0500

    add feature for indentation in if statement

--- checkExp.js
+++ checkExp.js
@@ -1,18 +1,17 @@
 //const expIf = new RegExp("ab");
-const expVarName = /^\D(\D|\d|-|_)*/; // equivalente a ^[a-zA-Z]([a-zA-Z]|[0-9]|-)*
+const expVarName = /^[a-z]+(\d|[a-z]|-|_)*$/; // equivalente a ^[a-zA-Z]([a-zA-Z]|[0-9]|-)*
 const expGetIfPar = /(?<vn>(\w+|\"(\w*\s*)*\"))\s*(?<op>(>|<|==|<=|>=))\s*(?<con>(\w+|\"(\w*\s*)*\"))/; //expresion obtener parametros if
-const expIfDecl = /if\s*\(\s*(\w+|\"(\w*\s*)*\")\s*(>|<|==|<=|>=)\s*(\w+|\"(\w*\s*)*\")\s*\)\:/; //exp declaracion if
-const expStrg = /^\"\w*|\s*\"/
-const expFunct = /^def [a-zA-Z]+\(([a-z]\w*,*)*\)\:$/
-const expWhile = /^while\s*\((\w*|\D*)*\):$/
+const expIfDecl = /if\s*\((\w*|\W*)*\)\:\n(\t|\s{4})(pass)$/; //exp declaracion if
+const expStrg = /^\"\w*|\s*\"/;
+const expFunct = /^def [a-zA-Z]+\(([a-z]\w*,*)*\)\:$/;
+const expWhile = /^while\s*\((\w*|\D*)*\):$/;
 //const input = 'if(abc==8):'
 
 function chcVarName(varName) {
   return expVarName.test(varName);
 }
 
-
-function getIfparam(syntax){
+function getIfparam(syntax) {
   let match = expGetIfPar.exec(syntax);
   let comp1 = match.groups.vn;
   let opert = match.groups.op;
@@ -21,36 +20,34 @@ function getIfparam(syntax){
   return [comp1, opert, comp2];
 }
 
-
-
-function validStrgs(inp){
-  return expStrg.test(inp)
+function validStrgs(inp) {
+  return expStrg.test(inp);
 }
 
 export function identifSyntx(input) {
   if (expIfDecl.test(input)) {
-    const [comp1, opert, conp2] = getIfparam(input)
-    
-
-    if(chcVarName(comp1) && chcVarName(conp2)){
-      return 'correct syntax for IF statemen'
-    }else if (validStrgs(comp1) && validStrgs(conp2) && opert=='==') {
-      return 'correct'
-    }else if (chcVarName(comp1) && validStrgs(conp2) && opert=='=='){
-      return 'correct'
-    }else if (validStrgs(comp1) && chcVarName(conp2) && opert=='=='){
-      return 'correct'
+    if (!expGetIfPar.test(input)) {
+      return "Error en condicional no declarada";
     }
-
-    return 'incorrect syntax for IF statemen'
-  }else if (expFunct.test(input)){
-    return 'Syntax correct for Function'
-  }else if (expWhile.test(input)){
-    return 'Syntax correct for While'
+    const [comp1, opert, conp2] = getIfparam(input);
+    if (chcVarName(comp1) && chcVarName(conp2)) {
+      return "correct syntax for IF statemen";
+    } else if (validStrgs(comp1) && validStrgs(conp2) && opert == "==") {
+      return "correct";
+    } else if (chcVarName(comp1) && validStrgs(conp2) && opert == "==") {
+      return "correct";
+    } else if (validStrgs(comp1) && chcVarName(conp2) && opert == "==") {
+      return "correct";
+    }
+    // todo: añadir condicional para numeros
+    return "incorrect syntax for IF statemen";
+  } else if (expFunct.test(input)) {
+    return "Syntax correct for Function";
+  } else if (expWhile.test(input)) {
+    return "Syntax correct for While";
   }
-  return 'not found statemen'
+  return "not found statemen";
 }
-
 
 //console.log(identifSyntx('if (b == "a"):'))
 //console.log(chcVarName('1a'))
--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const text = document.querySelector("#expr");
 const form = document.querySelector("#form");
 const resp = document.querySelector("#respons");
 
+text.addEventListener('keydown', function(e){
+  if (e.key == 'Tab'){
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    this.value = this.value.substring(0, start)+"\t"+this.value.substring(end);
+
+    this.selectionStart = this.selectionEnd = start + 1;
+  }
+})
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log(text.value);
