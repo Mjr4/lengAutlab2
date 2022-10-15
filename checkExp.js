bc93050de0deac99c69906ad0b14cae8d6a2102b
Mjr4 <estmvega4@gmail.com>
2022-10-15 01:35:11 -0500

    refactor and add features for while syntax

--- checkExp.js
+++ checkExp.js
@@ -1,72 +1,117 @@
 //const expIf = new RegExp("ab");
 const expVarName = /^[a-z]+(\d|[a-z]|-|_)*$/; // equivalente a ^[a-zA-Z]([a-zA-Z]|[0-9]|-)*
-const expGetIfPar = /(?<vn>(\w+|\"(\w*\s*)*\"))\s*(?<op>(>|<|==|<=|>=))\s*(?<con>(\w+|\"(\w*\s*)*\"))/; //expresion obtener parametros if
+const expGetIfPar =
+  /(?<vn>(\w+|\"(\w*\s*)*\"))\s*(?<op>(>|<|==|<=|>=))\s*(?<con>(\w+|\"(\w*\s*)*\"))/; //expresion obtener parametros if
 const expIfDecl = /if\s*\((\w*|\W*)*\)\:\n(\t|\s{4})(pass)$/; //exp declaracion if
 const expStrg = /^\"\w*|\s*\"/;
-const expFunct = /^def [a-z]+\((\w|,|\s)*\)\:$/;;
+const expFunct = /^def [a-z]+\((\w|,|\s)*\)\:$/;
 const expWhile = /^while\s*\((\w*|\D*)*\):$/;
 const expGetFunctPar = /\((?<p>(\w+|\s*|,)*\w+)*\)/;
+const expLogOp = /\((?<logOp>(true|false))\)/;
 //const input = 'if(abc==8):'
 
-function chcVarName(varName) {
+function isValidVarName(varName = "") {
   return expVarName.test(varName);
 }
 
-function getIfparam(syntax) {
+function getIfparam(syntax = "") {
   let match = expGetIfPar.exec(syntax);
-  let comp1 = match.groups.vn;
+  let param1 = match.groups.vn;
   let opert = match.groups.op;
-  let comp2 = match.groups.con;
+  let param2 = match.groups.con;
 
-  return [comp1, opert, comp2];
+  return [param1, opert, param2];
 }
 
-function validStrgs(inp) {
-  return expStrg.test(inp);
+function isValidStrgs(input = "") {
+  return expStrg.test(input);
 }
 
-export function identifSyntx(input) {
-  if (expIfDecl.test(input)) {
-    if (!expGetIfPar.test(input)) {
+function getFunctionParam(syntax = "") {
+  const param = expGetFunctPar.exec(syntax);
+  let paramList = param.groups.p.split(",");
+  return paramList;
+}
+
+function isValidIfDecl(syntax = "") {
+  return expIfDecl.test(syntax);
+}
+
+function arValidIfParam(input = "") {
+  return expGetIfPar.test(input);
+}
+
+function isValidFunctDecl(syntax = "") {
+  return expFunct.test(syntax);
+}
+
+function arValidFunctParam(input = "") {
+  return expGetFunctPar.test(input);
+}
+
+function isValidWhileDecl(syntax = "") {
+  return expWhile.test(syntax);
+}
+
+function isValidLogOp(syntax=""){
+  return expLogOp.test(syntax)
+}
+
+function getLogOp(syntax=""){
+  const logOp = expLogOp.exec(syntax);
+  return logOp.groups.logOp
+}
+
+function validateIfParam(param1, opert, param2) {
+  if (isValidVarName(param1) && isValidVarName(param2)) {
+    return "Correcta declaracion de sentencia If y nombres de variables";
+  } else if (isValidStrgs(param1) && isValidStrgs(param2) && opert == "==") {
+    return "Correcta declaracion de sentencia If y validacion de cadenas";
+  } else if (isValidVarName(param1) && isValidStrgs(param2) && opert == "==") {
+    return "Correcta declaracion de sentencia If, nombres de variables y validacion de cadenas";
+  } else if (isValidStrgs(param1) && isValidVarName(param2) && opert == "==") {
+    return "Correcta declaracion de sentencia If, nombres de variables y validacion de cadenas";
+  }
+  // todo: añadir condicional para numeros
+  return "Error de parametros";
+}
+
+function isValidParam(functParmList) {
+  let validParam = false;
+  functParmList.forEach((element) => {
+    if (expVarName.test(element)) {
+      validParam = true;
+    } else {
+      validParam = false;
+    }
+  });
+  return validParam;
+}
+
+export function identifSyntx(input = "") {
+  if (isValidIfDecl(input)) {
+    if (!arValidIfParam(input)) {
       return "Error en condicional no declarada";
     }
-    const [comp1, opert, conp2] = getIfparam(input);
-    if (chcVarName(comp1) && chcVarName(conp2)) {
-      return "correct syntax for IF statemen";
-    } else if (validStrgs(comp1) && validStrgs(conp2) && opert == "==") {
-      return "correct";
-    } else if (chcVarName(comp1) && validStrgs(conp2) && opert == "==") {
-      return "correct";
-    } else if (validStrgs(comp1) && chcVarName(conp2) && opert == "==") {
-      return "correct";
+    let [param1, opert, param2] = getIfparam(input);
+    return validateIfParam(param1, opert, param2);
+  } else if (isValidFunctDecl(input)) {
+    if (!arValidFunctParam(input)) {
+      return "Error de parametros";
     }
-    // todo: añadir condicional para numeros
-    return "incorrect syntax for IF statemen";
-  } else if (expFunct.test(input)) {
-    if (!expGetFunctPar.test(input)){
-      return "error de parametros"
+    let functParmList = getFunctionParam(input);
+    let validIfParam = isValidParam(functParmList);
+    return validIfParam
+      ? "Correcta declaracion de funcion"
+      : "Error de parametros";
+  } else if (isValidWhileDecl(input)) {
+    if(arValidIfParam(input) | isValidLogOp(input)) {
+      return "Syntaxis correcta para sentencia While";
     }
-    const param = expGetFunctPar.exec(input)
-    let param_a = param.groups.p
-    param_a = param_a.split(",")
-    let all_param = false
-    param_a.forEach(element => {
-      if(expVarName.test(element)){
-        all_param = true
-      }else{
-        all_param = false
-      }
-    });
-    if (all_param){
-      return "Syntax correct for Function";
-    }else{
-      return "Syntax incorrect for Function";
-    }
-  } else if (expWhile.test(input)) {
-    return "Syntax correct for While";
+    return "Error de parametros o operadores"
   }
-  return "not found statemen";
+  return "No se identifica la sintaxis introducida";
 }
 
 //console.log(identifSyntx('if (b == "a"):'))
-//console.log(chcVarName('1a'))
+//console.log(identifSyntx('if (1a>b):\n\tpass'));
